705db229035ba7e18853e988f3f5d38099be8964
lenmuel <lenmuel.dlrs@gmail.com>
2025-02-13 13:31:37 +0800

    updating

--- src/app/(content)/not-found.tsx
+++ src/app/(content)/not-found.tsx
@@ -5,10 +5,10 @@ export default function NotFound() {
     <div className="mx-auto grid h-full max-w place-items-center">
       <div className="text-center">
         <h1 className="text-4xl font-semibold text-foreground">
-          404 - Fant ikke siden
+          Looking for Something?
         </h1>
         <div className="mx-auto mt-8 w-fit">
-          <LinkButton href="/">Til forsiden</LinkButton>
+          <LinkButton href="/">Back to Homepage</LinkButton>
         </div>
       </div>
     </div>
