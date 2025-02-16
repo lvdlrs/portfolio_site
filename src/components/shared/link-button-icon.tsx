0d7cdeaf46cd2b56c45c77ecb50f187bfbeb4a67
lenmuel <lenmuel.dlrs@gmail.com>
2025-02-16 22:24:14 +0800

    updates

--- src/components/shared/link-button-icon.tsx
+++ src/components/shared/link-button-icon.tsx
@@ -19,11 +19,11 @@ export function LinkButtonIcon({
   return (
     <Link
       className={cn(
-        "rounded-md relative block text-center sm:w-fit whitespace-nowrap border-2 px-7 py-4 text-base/none font-medium transition-all",
+        "rounded-[35px] uppercase overflow-hidden relative flex items-center text-center sm:w-fit whitespace-nowrap border-2 pl-7 pr-16 py-5 text-base/none font-medium transition-all before:-z-[1] before:absolute before:top-0 before:right-0 before:left-0 before:bottom-0 before:translate-x-full before:transition-all before:duration-300 before:ease-out hover:before:translate-x-0 before:bg-foreground dark:before:bg-white",
         {
-          "hover:bg-foreground bg-primary border-transparent text-white":
+          "border-2 border-foreground hover:text-white dark:hover:text-foreground dark:border-white dark:text-white":
             variant === "default",
-          "text-primary hover:border-primary bg-background border-transparent":
+          "border-2 border-foreground bg-foreground text-white hover:text-foreground dark:hover:text-white dark:border-white dark:bg-white dark:text-foreground":
             variant === "ghost"
         },
       )}
@@ -32,7 +32,7 @@ export function LinkButtonIcon({
       <span>{props.children}</span>
       {props.icon?.metadata?.inlineSvg && (
         <span
-          className="w-full h-full flex items-center justify-center"
+          className="size-[55px] rounded-full flex items-center justify-center absolute top-0 -right-[2px] bg-foreground text-white"
           dangerouslySetInnerHTML={{
             __html: props.icon.metadata.inlineSvg,
           }}
